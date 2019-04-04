9421bbde0ba39ea9246ea29eedc2fe11f66bcd5c
Mamta Kumari <32190445+mamta103@users.noreply.github.com>
2019-04-04 07:54:38 +0530

    @onetoone delet query execution
    Hibernate one to one mapping. Delete row from dependent table

---
+++ entity/InstructorDetail.java
@@ -0,0 +1,79 @@
+package com.love2code.hibernate.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+	// annotate the class as an entity and map to the db class
+	
+	
+	//define the fields
+	
+	//annotate the fields with db columns names
+	
+	//create cnstructor
+	
+	//generate getter and setter
+	
+	//generate tostring
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="youtube_channel")
+	private String youtubeChannel;
+	
+	@Column(name="hobby")
+	private String hobby;
+	
+	public InstructorDetail(){
+		
+	}
+
+	public InstructorDetail(int id, String youtubeChannel, String hobby) {
+		super();
+		this.id = id;
+		this.youtubeChannel = youtubeChannel;
+		this.hobby = hobby;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getYoutubeChannel() {
+		return youtubeChannel;
+	}
+
+	public void setYoutubeChannel(String youtubeChannel) {
+		this.youtubeChannel = youtubeChannel;
+	}
+
+	public String getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
+	}
+	
+	
+
+}
---
+++ entity/Student.java
@@ -0,0 +1,76 @@
+package com.love2code.hibernate.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+	public Student() {
+		super();
+	}
+
+	@Id
+	@Column(name = "ID")
+	private int id;
+
+	@Column(name = "FirstName")
+	private String firstName;
+
+	@Column(name = "LastName")
+	private String lastName;
+
+	@Column(name = "Email")
+	private String email;
+
+	public Student(int id, String lastName, String firstName, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+	
+	
+
+}
